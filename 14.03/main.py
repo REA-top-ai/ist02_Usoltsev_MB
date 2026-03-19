c80956b2166ad240b450fb1f7ad2d7eb22413ff3
Amontapelir <130670742+Amontapelir@users.noreply.github.com>
2026-03-19 20:37:23 +0300

    Update and rename main.txt to main.py

---
+++ 14.03/main.py
@@ -0,0 +1,27 @@
+from modules.api_methods import get_everything
+import requests as r
+import pprint
+import json
+apiKey = '143d957cfd68407abf16ca96845956e0'
+url = 'https://newsapi.org/v2/everything'
+
+data = get_everything(apiKey, q='news', sortBy='publishedAt', language='ru')
+
+filtered_data = []
+
+for article in data['articles']:
+    artdict = {}
+    title = article['title']
+    artdict['title'] = title
+    urllink = article['url']
+    artdict['url'] = urllink
+    description = article['description']
+    artdict['description']= description
+    artdict['date'] = article['publishedAt']
+    
+    if (title != '[removed]') and (urllink is not None) and (len(description) >= 50):
+        filtered_data.append(artdict)
+    if len(filtered_data) == 50:
+        break
+
+print(filtered_data)
