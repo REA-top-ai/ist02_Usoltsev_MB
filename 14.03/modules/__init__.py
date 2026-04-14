8e41f7104e55b50c26a35bbc7d792e17a289f627
Amontapelir <130670742+Amontapelir@users.noreply.github.com>
2026-04-14 09:08:39 +0300

    Update __init__.py

--- 14.03/modules/__init__.py
+++ 14.03/modules/__init__.py
@@ -1,9 +1,13 @@
-from .API_methods import (
-    get_top_headlines
+from .api_methods import (
+    get_top_headlines,
+    get_everything,
+    get_sources
 )
 
 __all__ = [
-    'get_top_headlines'
+    'get_top_headlines',
+    'get_everything',
+    'get_sources'
 ]
 
 __version__ = '1.0.0'
